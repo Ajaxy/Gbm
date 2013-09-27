e6cc9c48afc8cda282000625e918b28364bfde11
Ajaxy <alx.zinchuk@gmail.com>
2013-09-27 17:22:28 +0400

    init staging

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function() {
+
+    /* Scroll Country List */
+    jQuery('.scroll-list').jScrollPane();
+
+
+    /* Slider */
+    $('#slides').slidesjs({
+        height: 328,
+        navigation: false
+      });
+});
