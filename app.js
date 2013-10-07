c038ab3060e9f8c3c72224d1deda4bd53f337818
Ajaxy <alx.zinchuk@gmail.com>
2013-10-07 17:24:49 +0400

    Выключаем статику в Node (прячем за nginx)

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 // development only
